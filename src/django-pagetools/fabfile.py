cdf2ef7ff3f940110720688bc13ea1bb520f8fa9
Your Name <you@example.com>
2014-01-14 19:25:41 +0100

    typos

--- src/django-pagetools/fabfile.py
+++ src/django-pagetools/fabfile.py
@@ -4,6 +4,6 @@ from fabric.api import lcd
 
 def prepare_deployment(branch_name):
     with lcd('~/workspaces/python/django-pagetools/pagetools/demo'):
-        local('python manage.py test myapp')
+        local('python manage.py test demo')
     local('git add -p && git commit')
     local('git checkout master && git merge ' + branch_name)
