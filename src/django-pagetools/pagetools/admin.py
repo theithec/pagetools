1bd418d77f9d9858b4577f37f4e796297f607e32
Tim Heithecker <tim.heithecker@gmail.com>
2014-05-18 13:51:21 +0200

    fieldset copy

--- src/django-pagetools/pagetools/admin.py
+++ src/django-pagetools/pagetools/admin.py
@@ -8,6 +8,16 @@ from django.contrib import admin
 
 from .settings import ADMIN_SET_LANG_FILTER
 
+
+def fieldset_copy(fieldset):
+    fscopy = (fieldset[0],
+              {'fields': [f for f in fieldset[1]['fields']]}
+             )
+    clzs = fieldset[1].get('classes')
+    if clzs:
+        fscopy[1]['classes'] = [c for c in clzs]
+    return fscopy
+
 class TinyMCEMixin(object):
     class Media:
         js = [
