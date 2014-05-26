673cc3c1bdae5fe4de7356b93f1f024943b7c2b0
Tim Heithecker <tim.heithecker@gmail.com>
2014-05-26 18:18:54 +0200

    move a lot

--- src/django-pagetools/pagetools/search/__init__.py
+++
@@ -1,6 +0,0 @@
-search_mods = (
-   # ( app.models.Model1,   ('title', 'content') ),
-   # ( app.models.Model2, ('title', 'content','footer') ),
-)
-
-extra_filter = lambda x:x
--- src/django-pagetools/pagetools/widgets/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .utils import get_areas_for_type, type_or_none
-
-
-def base_page(request=None):
-	d = {}
-	d['areas'] = get_areas_for_type(type_or_none('base'), {})
-	return d
-	
