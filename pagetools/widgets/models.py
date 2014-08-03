d4f1f9f96bb7af8a87e2a98e6b38abda297fd555
Tim Heithecker <tim.heithecker@gmail.com>
2014-08-03 19:12:21 +0200

    rm print

--- pagetools/widgets/models.py
+++ pagetools/widgets/models.py
@@ -57,7 +57,7 @@ class TemplateTagWidget(BaseWidget):
                 self.renderclasskey,
                 (None, None)
             )
-            print "mcname", modulename, clsname
+            #print "mcname", modulename, clsname
             module = importlib.import_module(modulename)
             try:
                 self.robj = getattr(module, clsname)()
