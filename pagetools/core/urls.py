28903747596032eed7b9815391ad4da682997c4c
Tim Heithecker <tim.heithecker@gmail.com>
2015-09-07 17:26:33 +0200

    fix admin menu entries (no tests)

--- pagetools/core/urls.py
+++ pagetools/core/urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls import patterns, url
 #from django.conf.urls.shortcut import urlpatterns
 
-
+urlpatterns = []
 if settings.DEBUG:
-    urlpatterns = [
+    urlpatterns += [
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
--- pagetools/menus/tests/test_admin.py
+++ pagetools/menus/tests/test_admin.py
@@ -4,9 +4,33 @@ Created on 15.12.2013
 @author: lotek
 '''
 from django.test.testcases import TestCase
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin.options import (
+        HORIZONTAL, VERTICAL, ModelAdmin, TabularInline,
+)
+from pagetools.menus.models import Menu, MenuManager, Link
+
+class MockRequest(object):
+    pass
 
 
-class Tests(TestCase):
+class MockSuperUser(object):
+    def has_perm(self, perm):
+        return True
+
+request = MockRequest()
+request.user = MockSuperUser()
+
+class MenuAdminTests(TestCase):
 
     def setUp(self):
-        pass
+        self.site = AdminSite()
+        self.menu = Menu.objects.add_root(title="menu1")
+        self.l1 = Link.objects.create(title="l1", url="#")
+        self.e1 = Menu.objects.add_child(self.menu,self.l1)
+
+    #todo rf before test
+    def test_change_entries_enabled(self):
+        ma = ModelAdmin(Menu, self.site)
+        F = ma.get_form(request, obj=self.menu)
+        f = F(instance=self.menu)
