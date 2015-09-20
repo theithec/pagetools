f88034a130b189ad3618a66a4ce9e83d1932a695
Tim Heithecker <tim.heithecker@gmail.com>
2015-09-20 17:38:50 +0200

    selection with dashboardmodule(0.2.9-5)

---
+++ pagetools/sections/static/js/nodetree.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    function load_pagenodes(slug){
+        var url = admin_pagenodesurl.replace('__SLUG__', slug);
+        $.ajax({
+            url: url,
+            success: function(data, textStatus, jqXHR){
+               $("#pagenodes").replaceWith(data);
+               $("#pagenodes").bonsai({'expandAll':true});        
+            }
+        });
+    }
+    $('#pagenode_page_chooser').change(function(evt){
+        load_pagenodes(this.value);
+    });
+    var selText = $('#pagenode_page_chooser option:selected').text();
+    if (selText && selText != ""){
+        load_pagenodes(selText);
+    }
+});
