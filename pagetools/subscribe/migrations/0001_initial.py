20984d0d6095fd1f057391796be2a824f868aecc
Tim Heithecker <tim.heithecker@gmail.com>
2015-12-08 20:02:12 +0100

    reset migrations again

---
+++ pagetools/subscribe/migrations/0001_initial.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import pagetools.subscribe.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='QueuedEmail',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('lang', models.CharField(max_length=20, blank=True, verbose_name='language', choices=settings.LANGUAGES)),
+                ('createdate', models.DateTimeField(verbose_name='Created on', auto_now_add=True)),
+                ('modifydate', models.DateTimeField(verbose_name='Last modified on', auto_now_add=True)),
+                ('senddate', models.DateTimeField(verbose_name='Send after', auto_now_add=True)),
+                ('subject', models.CharField(max_length=255, verbose_name='Subject', default='', blank=True)),
+                ('body', models.TextField(verbose_name='Body', default='', blank=True)),
+            ],
+            options={
+                'verbose_name': 'News-Mail',
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='SendStatus',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('status', models.IntegerField()),
+                ('queued_email', models.ForeignKey(to='subscribe.QueuedEmail')),
+            ],
+            options={
+                'verbose_name': 'Status',
+                'verbose_name_plural': 'Statuses',
+            },
+        ),
+        migrations.CreateModel(
+            name='Subscriber',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('lang', models.CharField(max_length=20, blank=True, verbose_name='language', choices=settings.LANGUAGES)),
+                ('is_activated', models.BooleanField(default=False)),
+                ('subscribtion_date', models.DateTimeField(auto_now_add=True)),
+                ('failures', models.IntegerField(default=0)),
+                ('key', models.CharField(max_length=32, default=pagetools.subscribe.models._mk_key)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.AddField(
+            model_name='sendstatus',
+            name='subscriber',
+            field=models.ForeignKey(to='subscribe.Subscriber'),
+        ),
+    ]
---
+++ pagetools/widgets/migrations/0001_initial.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+from pagetools.widgets import settings as widget_settings
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ContentWidget',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=128, blank=True)),
+                ('name', models.SlugField(verbose_name='name', unique=True)),
+                ('content', models.TextField(verbose_name='Content')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='PageType',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('name', models.CharField(max_length=128, verbose_name='Name')),
+                ('parent', models.ForeignKey(to='widgets.PageType', blank=True, null=True)),
+            ],
+            options={
+                'verbose_name': 'Pagetype',
+                'verbose_name_plural': 'Pagetypes',
+            },
+        ),
+        migrations.CreateModel(
+            name='PageTypeDescription',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('lang', models.CharField(max_length=20, blank=True, verbose_name='language', choices=settings.LANGUAGES)),
+                ('description', models.CharField(max_length=156, help_text='Description (for Metatag/seo)', blank=True, verbose_name='Description')),
+                ('pagetype', models.ForeignKey(to='widgets.PageType')),
+            ],
+            options={
+                'verbose_name': 'Pagetype-Description',
+                'verbose_name_plural': 'Pagetype-Descriptions',
+            },
+        ),
+        migrations.CreateModel(
+            name='TemplateTagWidget',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=128, blank=True)),
+                ('name', models.SlugField(verbose_name='name', unique=True)),
+                ('renderclasskey', models.CharField(max_length=255, choices=sorted([(w, w) for w in widget_settings.TEMPLATETAG_WIDGETS]))),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='TypeArea',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('lang', models.CharField(max_length=20, blank=True, verbose_name='language', choices=settings.LANGUAGES)),
+                ('area', models.CharField(max_length=64, choices=sorted([(a) for a in widget_settings.AREAS ]))),
+                ('type', models.ForeignKey(to='widgets.PageType')),
+            ],
+            options={
+                'verbose_name': 'Pagetype-Area',
+                'verbose_name_plural': 'Pagetype-Areas',
+            },
+        ),
+        migrations.CreateModel(
+            name='WidgetInArea',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('object_id', models.PositiveIntegerField()),
+                ('position', models.PositiveIntegerField()),
+                ('enabled', models.BooleanField(verbose_name='enabled', default=False)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('typearea', models.ForeignKey(related_name='widgets', to='widgets.TypeArea')),
+            ],
+            options={
+                'ordering': ['position'],
+            },
+        ),
+        migrations.AlterUniqueTogether(
+            name='typearea',
+            unique_together=set([('area', 'type', 'lang')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='pagetypedescription',
+            unique_together=set([('pagetype', 'lang')]),
+        ),
+    ]
