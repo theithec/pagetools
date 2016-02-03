502fdc18821a87ac3efd1be4eb40dc4057574646
Tim Heithecker <tim.heithecker@gmail.com>
2016-02-03 13:59:54 +0100

    fix demo indexview

--- pagetoolsdemo1/main/views.py
+++ pagetoolsdemo1/main/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.shortcuts import render
 from pagetools.sections.views import BaseAjaxNodeView, BaseNodeView
 from .models import SectionPage, PageNode
 
@@ -13,9 +11,8 @@ class IndexView(NodeView):
     template_name = "sections_base.html"
 
     def get_object(self, *args, **kwargs):
-        self.object2 = SectionPage.objects.filter(
+        self.object = SectionPage.objects.filter(
             content_type_pk=SectionPage.get_contenttype_pk()).first()
-        self.object = SectionPage.objects.first()
         return self.object
 
 
