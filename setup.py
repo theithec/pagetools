877cf8b7d44f53122b0864f82d77c006b0e8096e
Tim Heithecker <tim.heithecker@gmail.com>
2016-10-26 20:22:34 +0200

    README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 # from distutils.core import setup
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 from version import get_git_version
