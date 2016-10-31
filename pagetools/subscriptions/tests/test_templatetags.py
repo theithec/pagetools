32b78d5b77c9b31ad17d1141280968fc3f20bc6a
Tim Heithecker <tim.heithecker@gmail.com>
2016-10-31 13:38:41 +0100

    renaming

--- pagetools/subscriptions/tests/test_templatetags.py
+++ pagetools/subscriptions/tests/test_templatetags.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 
-from pagetools.subscriptions.templatetags.subscriptions_tags import do_news_subscribtion
+from pagetools.subscriptions.templatetags.subscriptions_tags import do_subscribe_node
 
 class TTTestCase(TestCase):
     def test_subscripton_node(self):
-        nsn = do_news_subscribtion(None, None)
+        nsn = do_subscribe_node(None, None)
         self.assertTrue('name="email"' in nsn.render({}))
