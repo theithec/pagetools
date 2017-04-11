bf74f425b54ce41730905865f55a70da8fb9484f
Tim Heithecker <tim.heithecker@gmail.com>
2017-04-11 14:48:46 +0200

    fix search test extra_filter

--- pagetools/search/tests/test_views.py
+++ pagetools/search/tests/test_views.py
@@ -11,14 +11,16 @@ from django.utils.text import slugify
 from django.core.urlresolvers import reverse
 from pagetools.pages.models import Page
 import pagetools.search
+import pagetools.search.views
 from pagetools.core.settings import STATUS_PUBLISHED
 
 
 pagetools.search.search_mods = (
     (Page, ('title', 'content'), {'replacements': 'content'}),
 )
-
+pagetools.search.views.SearchResultsView._search_mods = pagetools.search.search_mods
 pagetools.search.extra_filter = lambda x: x.filter(status=STATUS_PUBLISHED)
+pagetools.search.views.extra_filter = lambda x: x.filter(status=STATUS_PUBLISHED)
 # search.extra_filter = lambda x: x
 
 
