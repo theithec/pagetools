f47053e07c22903334e3393c0d6c92879e709c05
Tim Heithecker <tim.heithecker@gmail.com>
2020-02-16 12:44:21 +0100

    Add subcsripions/apps

---
+++ pagetools/subscriptions/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class SubscriptionsConfig(AppConfig):
+    name = 'pagetools.subscriptions'
+    verbose_name = "Subscriptions"
+
+    from .forms import SubscribeForm
+    subscribe_form = SubscribeForm
+
